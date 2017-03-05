106b0624be7498be391905101a98330d06a16c80
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-03-05 18:15:34 +0100

    Renamed basic commands

--- cmd/connect.go
+++ cmd/connect.go
@@ -20,9 +20,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// updateCmd represents the update command
-var updateCmd = &cobra.Command{
-	Use:   "update",
+// connectCmd represents the connect command
+var connectCmd = &cobra.Command{
+	Use:   "connect",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,21 +32,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("update called")
+		fmt.Println("connect called")
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(updateCmd)
+	RootCmd.AddCommand(connectCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// updateCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// connectCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// updateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// connectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
