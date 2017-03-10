4b5715e2176a619a57bf60930f70bd12dfcd1b31
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-03-10 22:21:15 +0100

    API: Substitued dummy uri for config value

--- cmd/dump.go
+++ cmd/dump.go
@@ -15,7 +15,6 @@ import (
     log "github.com/Sirupsen/logrus"
     "github.com/icot/dbod/api"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // dumpCmd represents the dump command
@@ -39,13 +38,10 @@ func init() {
 
 func dump (cmd *cobra.Command, args []string) {
 	log.Debug("dump called")
-	config := viper.GetViper() // Find and read the config file
 	if len(args) != 1 {
 		log.Fatal("Error: please run $ dbod " + dumpCmd.Use )
 	}
 	instance := args[0]
-	url := fmt.Sprintf("%s/%s/metadata", config.Get("api_instance_uri"), instance)
-    log.Debug("API URL:" + url)
     var metadata map[string] interface {}
     metadata = api.GetInstance(instance)
     str, _ := json.MarshalIndent(metadata, "", "  ")
