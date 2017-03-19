41df5bb64bfb24f91795c430ad255d2fcae25510
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-03-19 20:49:14 +0100

    API: Refactored to expose the *http.Client for testing

--- api/api.go
+++ api/api.go
@@ -9,66 +9,76 @@
 package api
 
 import (
-    "crypto/tls"
-    "crypto/x509"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "encoding/json"
-    "reflect"
-	
-    log "github.com/Sirupsen/logrus"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-type Instance map[string] interface{}
+type Instance map[string]interface{}
 
-func initHTTPClient() *http.Client {
-	config := viper.GetViper()
-    tls_conf := config.Sub("tls")
-    ca_file, err := ioutil.ReadFile(tls_conf.GetString("ca"))
-    if err != nil {
-        log.Fatal(err)
-    }
-    roots := x509.NewCertPool()
-    ok := roots.AppendCertsFromPEM([]byte(ca_file))
-    if !ok {
-        log.Fatal("Failed to parse CA certificate")
-    }
-    
-    // Initialize Transport and HTTP Client
-    tlsConf := &tls.Config{RootCAs: roots}
-    tr := &http.Transport{TLSClientConfig: tlsConf}
-    return &http.Client{Transport: tr}
+var Client *http.Client
+
+func init() {
+}
+
+func GetClient() *http.Client {
+	if Client == nil {
+		log.Debug("Initializing API Client")
+		config := viper.GetViper()
+		tls_conf := config.Sub("tls")
+		log.Debug("CA file: " + tls_conf.GetString("ca"))
+		ca_file, err := ioutil.ReadFile(tls_conf.GetString("ca"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		roots := x509.NewCertPool()
+		ok := roots.AppendCertsFromPEM([]byte(ca_file))
+		if !ok {
+			log.Fatal("Failed to parse CA certificate")
+		}
+
+		// Initialize Transport and HTTP Client
+		tlsConf := &tls.Config{RootCAs: roots}
+		tr := &http.Transport{TLSClientConfig: tlsConf}
+		Client = &http.Client{Transport: tr}
+	} else {
+		log.Debug("Pre-existing API Client")
+	}
+	log.Debug(fmt.Sprintf("API Client[%p]", Client))
+	return Client
 }
 
 func GetInstance(instance string) Instance {
 
-    log.Debug("Dumping " + instance)
-    // Find and read the config file
+	log.Debug("Fetching " + instance)
+	// Find and read the config file
 	config := viper.GetViper()
 	url := fmt.Sprintf("%s/%s/metadata", config.Get("api_instance_uri"), instance)
-    log.Debug("API URL:" + url)
-
-    client := initHTTPClient()
-    res, err := client.Get(url)
+	log.Debug("API URL:" + url)
+	client := GetClient()
+	res, err := client.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-    body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-    var resp = make(map[string] []Instance, 0)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	var resp = make(map[string][]Instance, 0)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-        log.Debug(reflect.TypeOf(resp))
-        log.Debug(string(body))
-        json.Unmarshal(body, &resp)
-    }
+		log.Debug(reflect.TypeOf(resp))
+		log.Debug(string(body))
+		json.Unmarshal(body, &resp)
+	}
 
-    // API Response is an array of JSON Objects
-    return resp["response"][0]
+	// API Response is an array of JSON Objects
+	return resp["response"][0]
 
 }
-
